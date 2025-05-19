02edef9af535b99483dc2fdcf1a2e4ff5c03f76a
mayas-hasan <myas.hasan.890@gmail.com>
2025-05-20 00:49:30 +0300

    Update code1.sh

--- code1.sh
+++ code1.sh
@@ -19,34 +19,3 @@ if [ "$remaining_days" -eq 0 ]; then
 echo "Remaining days cannot be zero"    # Print warning if user enters 0 for remaining days
 exit 1       # Exit the script to prevent further execution
 fi
-
-
-lessons_per_day=$((total_lessons / remaining_days))
-extra_lessons=$((total_lessons % remaining_days)) 
-
-
-output_file="study_plan.txt" 
-
-echo "Study plan for $total_lessons lessons over $remaining_days days" > "$output_file"
-awk -v total="$total_lessons" -v days="$remaining_days" 'BEGIN{print "Average lessons per day: " total / days }'>> "$output_file"
-
-for (( day=1; day<=remaining_days; day++ )); do
-if [ $day -le "$extra_lessons" ]; then
-lessons=$((lessons_per_day + 1))
-else 
-lessons=$lessons_per_day
-fi
-echo "Day $day: --> Study $lessons lesson(s)"
-echo "Day $day: --> Study $lessons lessons(s)" >> "$output_file"
-done 
-echo ""
-echo "Lines formatted with SED:"
-sed 's/lesson(s)/lesson(s) - stay focused!/' "$output_file"
-echo "good luck with your study!"
-echo "Days with more than two lessons:"
-grep -E "Study [3-9]" "$output_file"
- 
-echo ""
-echo "summary using cut command:"
-cut -d":" -f1,3 "$output_file" | grep "Study"
-
